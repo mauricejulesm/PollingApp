facffc57b13f92423e698ef666a2dc0bdf0fa854
Maurice JM <julesmaurice.mulisa@gmail.com>
2020-04-30 22:07:58 +0200

    improved views to use django generic views

--- Polling/urls.py
+++ Polling/urls.py
@@ -1,11 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from pollsapp.views import index
+from pollsapp import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('polls/', include("pollsapp.urls")),
 ]
