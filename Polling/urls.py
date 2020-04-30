cd41863d7a414e547a894cb469e58cd9fdf2aa62
Maurice JM <julesmaurice.mulisa@gmail.com>
2020-04-30 16:18:15 +0200

    changed the index view to query the database

--- Polling/urls.py
+++ Polling/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from pollsapp.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
     path('polls/', include("pollsapp.urls")),
 ]
